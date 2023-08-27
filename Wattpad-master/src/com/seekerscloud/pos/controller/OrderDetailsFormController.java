513f0e9f4a2b5eaaec7ce9749778a208921d8bf5
suren-11 <presanth11@gmail.com>
2023-08-27 23:46:33 +0530

    completed forms with sql codes

--- Wattpad-master/src/com/seekerscloud/pos/controller/OrderDetailsFormController.java
+++ Wattpad-master/src/com/seekerscloud/pos/controller/OrderDetailsFormController.java
@@ -1,6 +1,7 @@
 package com.seekerscloud.pos.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import com.seekerscloud.pos.db.DBConnection;
 import com.seekerscloud.pos.db.Database;
 import com.seekerscloud.pos.model.CartItem;
 import com.seekerscloud.pos.model.Customer;
@@ -15,6 +16,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 
 public class OrderDetailsFormController {
@@ -41,44 +45,95 @@ public class OrderDetailsFormController {
 
 
     public void loadData(String orderId){
-        if (orderId==null){
-            removeUi();
-            return;
-        }
-
-       Order o= Database.orderTable.stream().filter(e->e.getOrderId().equals(orderId))
-                .findFirst().orElse(null);
-
-        if (o!=null){
-
-            Customer c= Database.customerTable.stream().filter(e->e.getId().equals(o.getCustomer()))
-                    .findFirst().orElse(null);
-            if (c!=null){
-                //===============
-                txtId.setText(c.getId());
-                txtName.setText(c.getName());
-                txtAddress.setText(c.getAddress());
-                txtSalary.setText(String.valueOf(c.getSalary()));
-                //===============
-                txtOrderId.setText(o.getOrderId());
-                txtDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(o.getPlacedDate()));
-                txtCost.setText(String.valueOf(o.getTotal()));
-                //===============
-                ObservableList<ItemDetailsTM> tmList = FXCollections.observableArrayList();
-                for (CartItem item: o.getItems()
-                     ) {
-                    tmList.add(new ItemDetailsTM(item.getCode(),
-                            item.getUnitPrice(), item.getQty(), (item.getQty())* item.getUnitPrice()));
-                }
-                tblItemDetails.setItems(tmList);
-                //===============
-
-            }else{
+        try {
+            if (orderId == null) {
                 removeUi();
+                return;
             }
 
-        }else{
-            removeUi();
+
+            String sql1 = "SELECT customer,date FROM `order` WHERE  orderId = ?";
+            PreparedStatement statement1 = DBConnection.getInstance().getConnection().prepareStatement(sql1);
+            statement1.setString(1,orderId);
+            ResultSet set1 = statement1.executeQuery();
+
+            if (set1.next()){
+                String customerId = set1.getString(1);
+                String date = set1.getString(2);
+
+                String sql2 = "SELECT * FROM Customer WHERE id = ?";
+                PreparedStatement statement2 = DBConnection.getInstance().getConnection().prepareStatement(sql2);
+                statement2.setString(1, customerId);
+                ResultSet set2 = statement2.executeQuery();
+                if (set2.next()){
+                    txtId.setText(customerId);
+                    txtName.setText(set2.getString(2));
+                    txtAddress.setText(set2.getString(3));
+                    txtSalary.setText(set2.getString(4));
+
+                    txtOrderId.setText(orderId);
+                    txtDate.setText(date);
+
+                    String sql3 = "SELECT totalCost FROM `Order` WHERE orderId= ?";
+                    PreparedStatement statement4 = DBConnection.getInstance().getConnection().prepareStatement(sql3);
+                    statement4.setString(1,orderId);
+                    ResultSet set4 = statement4.executeQuery();
+
+                    if (set4.next()){
+                        double totalCost = set4.getDouble(1);
+                        String sql = "SELECT o.orderId, d.itemCode, d.orderId, d.unitPrice, d.qty FROM `Order` o INNER JOIN `Order Details` d ON o.orderId = d.orderId AND o.orderId = ?";
+                        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement(sql);
+                        statement.setString(1, orderId);
+                        ResultSet set = statement.executeQuery();
+                        ObservableList<ItemDetailsTM> tmList = FXCollections.observableArrayList();
+
+                        while (set.next()) {
+                            double tempUnitPrice = set.getDouble(4);
+                            int tempQty = set.getInt(5);
+                            double tempTotal = tempUnitPrice * tempQty;
+                            tmList.add(new ItemDetailsTM(set.getString(2), tempUnitPrice, tempQty, tempTotal));
+                            txtCost.setText(String.valueOf(totalCost));
+                        }
+                        tblItemDetails.setItems(tmList);
+                    }
+                }
+            /*Order o = Database.orderTable.stream().filter(e -> e.getOrderId().equals(orderId))
+                    .findFirst().orElse(null);
+
+            if (o != null) {
+
+                Customer c = Database.customerTable.stream().filter(e -> e.getId().equals(o.getCustomer()))
+                        .findFirst().orElse(null);
+                if (c != null) {
+                    //===============
+                    txtId.setText(c.getId());
+                    txtName.setText(c.getName());
+                    txtAddress.setText(c.getAddress());
+                    txtSalary.setText(String.valueOf(c.getSalary()));
+                    //===============
+                    txtOrderId.setText(o.getOrderId());
+                    txtDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(o.getPlacedDate()));
+                    txtCost.setText(String.valueOf(o.getTotal()));
+                    //===============
+
+                    for (CartItem item : o.getItems()
+                    ) {
+                        tmList.add(new ItemDetailsTM(item.getCode(),
+                                item.getUnitPrice(), item.getQty(), (item.getQty()) * item.getUnitPrice()));
+                    }
+                    tblItemDetails.setItems(tmList);
+                    //===============
+
+                } else {
+                    removeUi();
+                }
+
+            } else {
+                removeUi();
+            }*/
+            }
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
         }
 
 
