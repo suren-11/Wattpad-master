f8420623c8ef9b17e4bce43a26c7cbe4bd79b659
suren-11 <presanth11@gmail.com>
2023-08-29 14:13:28 +0530

    Created CrudDao CustomerDao, ItemDao

---
+++ Wattpad-master/src/com/seekerscloud/pos/dao/custom/ItemDao.java
@@ -0,0 +1,7 @@
+package com.seekerscloud.pos.dao.custom;
+
+import com.seekerscloud.pos.dao.CrudDao;
+import com.seekerscloud.pos.entity.Product;
+
+public interface ItemDao extends CrudDao<Product, String> {
+}
